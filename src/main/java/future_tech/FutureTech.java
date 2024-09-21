22891fba4990a8c806245b4910d1b754d4e299de
MochiMochi-752 <naka.mochi11@gmail.com>
2024-09-21 14:45:53 +0900

    基本的なことをした

---
+++ src/main/java/future_tech/FutureTech.java
@@ -0,0 +1,23 @@
+package future_tech;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import org.slf4j.Logger;
+
+public class FutureTech {
+
+    public static final String MOD_ID = "future_tech";
+    public static final String MOD_NAME = "Future Tech";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public FutureTech(IEventBus modBus, ModContainer container) {
+        modBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        LOGGER.info(MOD_NAME);
+    }
+
+}
